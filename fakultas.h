54f258073a82b72389f80d4dd638726a023dccbf
NIZAR RASYIID <nizarrasyiid1221@gmail.com>
2023-12-25 16:55:05 +0700

    first commit

---
+++ fakultas.h
@@ -0,0 +1,13 @@
+#ifndef FAKULTAS_H_INCLUDED
+#define FAKULTAS_H_INCLUDED
+#include <iostream>
+#define first(L) L.first
+#define last(L) L.last
+#define next(P) P->next
+#define info(P) P->info
+#define prev(P) P->prev
+using namespace std;
+
+
+
+#endif
